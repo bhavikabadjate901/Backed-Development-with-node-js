f51cc57e81b9754de986763456905779de1a9333
Bhavika Badjate <64325998+bhavikabadjate901@users.noreply.github.com>
2021-09-14 22:28:12 +0530

    coustom middleware, Req params

--- day4/Rest_API_Express/src/server.js
+++ day4/Rest_API_Express/src/server.js
@@ -1,14 +1,24 @@
 import express from "express"
 import morgan from "morgan";
-// import {json, urlencoded} from "body-parser";
+import {json, urlencoded} from "body-parser";
+const router = express.Router();
 const app = express();
 
 
 app.use(morgan('dev'));
+app.use(json());
+app.use(urlencoded({extended:true}))
+app.use('/api/v1', router);
 
-app.get('/', (req,res)=>{
-    console.log(req.body);
-    res.send({message:'OK'})
+
+const customLogger = (req,res,next) =>{
+    console.log("Logger Incoming");
+    console.log(req.body)
+    next();
+}
+
+app.get('/', (req,res)=>{ 
+    res.send({message:'OK GET'})
 })
 
 export const start = () =>{
@@ -18,9 +28,43 @@ export const start = () =>{
     
 }
 
-app.post('/', (req,res)=>{
+app.post('/', [customLogger, customLogger], (req,res)=>{ // customLogger is a middleware, which runs before funtion
     console.log(req.body);
     res.send({message:'OK POST'})
 })
+
+// router.get('/post', (req,res) =>{
+//     res.send({message:"Router OK GET"})
+// })
+
+// router.post('/post', (req,res) =>{
+//     res.send({message:"Router OK POST"})
+// })
+
+
+router
+    .route('/post')
+    .get((req,res) =>{
+        res.send({message:"Router OK GET"})
+    })
+    .post((req,res) =>{
+        res.send({message:"Router OK POST"})
+    })
+    
+router
+    .route('/post/:id/:num')
+    .get((req,res) =>{
+        res.send({message:"Router OK GET"})
+    })
+    .put((req,res) =>{
+        console.log(req.params)
+        res.send({message:"Router OK PUT"})
+    })
+    .patch((req,res) =>{
+        res.send({message:"Router OK PATCH"})
+    })
+    .delete((req,res) =>{
+        res.send({message:"Router OK DELETE"})
+    });
 
  
