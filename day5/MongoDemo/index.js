55227a685b3b0d1d5f6b648fe9ce64108f5aa528
Bhavika Badjate <64325998+bhavikabadjate901@users.noreply.github.com>
2021-09-15 23:35:12 +0530

    Added timestamp in schema

--- day5/MongoDemo/index.js
+++ day5/MongoDemo/index.js
@@ -13,14 +13,16 @@ const blog = new Mongoose.Schema({
     description: String,
 
     tags: [{type: String}]
-})
+}, {timestamps:true}
+
+)
 
 const Blog = Mongoose.model('blog', blog);
 
 
 connect()
 .then(async connection => {
-     const b = await Blog.create({title: 'Learn Node', description: 'Learning is the constant in your life' });
+     const b = await Blog.create({title: 'Learn Mongo', description: 'Learning Mongo is amazing' });
      console.log(b);
 })
 .catch(err => console.error(err))
